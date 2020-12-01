382ef74350879876be6b0a8b2a6fc1a684a356fd
erniker <josepablomedinagrande@hotmail.com>
2020-12-01 11:22:36 +0100

    Create Login and SingUp components

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <h1>Desde Login</h1>;
+  // Login State
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  // User extract
+  const { email, password } = user;
+
+  const onChangeLogin = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //Login Button
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // Empty field validation
+
+    // end to action
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesión</h1>
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Enail</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu Email"
+              value={email}
+              onChange={onChangeLogin}
+            ></input>
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              value={password}
+              onChange={onChangeLogin}
+            ></input>
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+        <Link className="enlace-cuenta" to={"/sing-up"}>
+          Crear cuenta
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/auth/SingUp.js
+++ src/components/auth/SingUp.js
@@ -1,7 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SingUp = () => {
-  return <h1>Desde SingUp</h1>;
+  // SingUp State
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirm: "",
+  });
+
+  // SingUp extract
+  const { name, email, password, confirm } = user;
+
+  const onChangeSingUp = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //SingUp Button
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // Empty field validation
+
+    // Minimum of 6 characters for password
+
+    // Check password and confirmPassword are equal
+
+    // Send to action
+  };
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Crear cuenta</h1>
+        <form onSubmit={onSubmit}>
+          <div className="campo-form">
+            <label htmlFor="name">Nombre</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Tu Nombre"
+              value={name}
+              onChange={onChangeSingUp}
+            ></input>
+          </div>
+          <div className="campo-form">
+            <label htmlFor="email">Enail</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu Email"
+              value={email}
+              onChange={onChangeSingUp}
+            ></input>
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              value={password}
+              onChange={onChangeSingUp}
+            ></input>
+          </div>
+          <div className="campo-form">
+            <label htmlFor="confirm">Confirmar Password</label>
+            <input
+              type="password"
+              id="confirm"
+              name="confirm"
+              placeholder="Repite tu Password"
+              value={confirm}
+              onChange={onChangeSingUp}
+            ></input>
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Registrarme"
+            />
+          </div>
+        </form>
+        <Link className="enlace-cuenta" to={"/"}>
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SingUp;
