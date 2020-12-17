c840152b93691e3f6690ce75cb1186242eec8917
erniker <josepablomedinagrande@hotmail.com>
2020-12-17 21:42:19 +0100

    working on User business logic

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const dbConect = require("./config/db");
 
 // Create server
 const app = express();
 
+// Conect to db
+dbConect();
+
+// Enable express.json
+app.use(express.json({ extended: true }));
+
 // App port
 const PORT = process.env.PORT || 4000;
+
+// Inmport routes
+app.use("/api/users", require("./routes/users"));
 
 // Run app
 app.listen(PORT, () => {
---
+++ server/routes/users.js
@@ -0,0 +1,9 @@
+// Route for creating user
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+// Create an user
+// api/users
+router.post("/", userController.createUser);
+module.exports = router;
