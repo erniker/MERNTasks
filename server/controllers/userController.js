b82fa7120d61a84acd329c7dc9ce9e5a4aadcbb1
erniker <josepablomedinagrande@hotmail.com>
2020-12-18 21:36:33 +0100

    working on user creation validation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,35 @@
-exports.createUser = (req, res) => {
-  console.log(req.body);
+const User = require("../models/User");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
+
+exports.createUser = async (req, res) => {
+  // Check if there are errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  // Extract email and password
+  const { email, password } = req.body;
+
+  try {
+    // Check if user doesnt exist
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(422).json({ msg: "User alredy exists" });
+    }
+    // Create new user
+    user = new User(req.body);
+
+    // Hashing password
+    const salt = await bcryptjs.genSalt(10);
+    user.password = await bcryptjs.hash(password, salt);
+
+    // Save new user
+    await user.save();
+    res.status(201).json({ msg: "User created" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Error creating user");
+  }
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,20 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const { check } = require("express-validator");
 
 // Create an user
 // api/users
-router.post("/", userController.createUser);
+router.post(
+  "/",
+  [
+    check("name", "The name field is required").not().isEmpty(),
+    check("email", "A valid email is required").isEmail(),
+    check(
+      "password",
+      "Please enter a password at least 8 character and contain At least one uppercase, one lower case and one special character."
+    ).isStrongPassword(),
+  ],
+  userController.createUser
+);
 module.exports = router;
