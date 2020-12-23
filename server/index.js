dd8eb97b07e81b453fc79dcd5f5767f9c9ad1689
erniker <josepablomedinagrande@hotmail.com>
2020-12-23 22:13:56 +0100

    last updates

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // App port
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 // Inmport routes
 app.use("/api/users", require("./routes/users"));
@@ -24,6 +24,6 @@ app.use("/api/projects", require("./routes/projects"));
 app.use("/api/tasks", require("./routes/tasks"));
 
 // Run app
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
